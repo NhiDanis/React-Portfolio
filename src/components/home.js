dd43f09fe48d1490e106dfa53f7de354e54af0d3
Nhi Danis <a.nhi1001@gmail.com>
2021-03-01 17:35:35 -0800

    commit home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line
 import React, { Component } from 'react';
+import { Cell, Grid } from 'react-mdl';
 
 class Home extends Component {
     render() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div style={{width:'100%', margin:'auto', height: '100%'}}>
+            <Grid className="home-grid" style={{height:'100%'}}>
+                <div className="banner">
+                    <h1>Hello, Welcome to My Portfolio!!</h1>
+                    <h3> I'm a Full-Stack Developer </h3>
+
+                 </div>
+
+
+            </Grid>
         </div>
     );
     };
